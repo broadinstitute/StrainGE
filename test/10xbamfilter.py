0b79e628dfb39dc2ff7bbba7a1342c2c68a346d1
Bruce Walker <bruce@appliedinvention.com>
2017-06-09 14:45:44 -0400

    Filter 10x alignment file by alignment score for OTU41 experiment.

---
+++ test/10xbamfilter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+
+import sys
+import pysam
+
+barcodes = []
+
+# pass 1: collect barcodes with good alignments
+with pysam.AlignmentFile(sys.argv[1], "rb") as bam:
+    for read in bam:
+        barcode = read.get_tag("BX") if read.has_tag("BX") else None
+        if barcode and read.has_tag("AS") and read.get_tag("AS") > -10:
+            barcodes.append(barcode)
+
+# pass 2: copy reads with good alignment barcodes
+with pysam.AlignmentFile(sys.argv[1], "rb") as bam:
+    with pysam.AlignmentFile(sys.argv[2], "wb", template=bam) as out:
+        for read in bam:
+            barcode = read.get_tag("BX") if read.has_tag("BX") else None
+            if barcode in barcodes:
+                out.write(read)
+
+
+#    output = pysam.AlignmentFile(sys.argv, "wb", template=bt2sam)

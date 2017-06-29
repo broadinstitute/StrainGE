683759b151520f7cf47a7ba9e5d1741bc73ce1d0
Bruce Walker <bruce@appliedinvention.com>
2017-06-29 17:13:55 -0400

    10xBamToFastq

---
+++ seqtools/10xBamToFastq.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+import pysam
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+
+readlist = []
+
+with open(sys.argv[2], "w") as fastq:
+    with pysam.AlignmentFile(sys.argv[1], "rb") as bam:
+        for read in bam:
+            barcode = read.get_tag("BX") if read.has_tag("BX") else ""
+            seq = Seq(read.query_sequence)
+            record = SeqRecord(Seq(read.query_sequence), id=read.query_name, description=barcode)
+            record.letter_annotations['phred_quality'] = read.query_qualities
+            readlist.append(record)
+    readlist.sort(lambda a, b: cmp(a.description, b.description) or cmp(a.id, b.id) or cmp(len(a), len(b)))
+    SeqIO.write(readlist, fastq, "fastq")
+
+for i in xrange(0, len(readlist), 2):
+    assert readlist[i].name == readlist[i+1].name

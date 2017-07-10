931eefc8f7898d2f04ed30a49bbd91d783df0204
Bruce Walker <bruce@appliedinvention.com>
2017-07-10 19:43:08 -0400

    tweak filtering

--- seqtools/10xbamfilter.py
+++ seqtools/10xbamfilter.py
@@ -4,21 +4,32 @@
 import sys
 import pysam
 
-barcodes = []
+goodbarcodes = []
+poorbarcodes = []
 
 # pass 1: collect barcodes with good alignments
 with pysam.AlignmentFile(sys.argv[1], "rb") as bam:
     for read in bam:
         barcode = read.get_tag("BX") if read.has_tag("BX") else None
-        if barcode and read.has_tag("AS") and read.get_tag("AS") > -10:
-            barcodes.append(barcode)
+        if barcode and read.has_tag("NM") and read.has_tag("AS"):
+            pos = read.get_reference_positions(full_length=True)
+            #minpos = min(pos)
+            #maxpos = max(pos)
+            #conserved = maxpos > 800 and maxpos < 1000 or minpos > 800 and minpos < 1000
+            conserved = False
+            #mismatches = read.get_tag("NM")
+            score = read.get_tag("AS")
+            if score > -10 and not conserved:
+                goodbarcodes.append(barcode)
+            elif score <= -10:
+                poorbarcodes.append(barcode)
 
 # pass 2: copy reads with good alignment barcodes
 with pysam.AlignmentFile(sys.argv[1], "rb") as bam:
     with pysam.AlignmentFile(sys.argv[2], "wb", template=bam) as out:
         for read in bam:
             barcode = read.get_tag("BX") if read.has_tag("BX") else None
-            if barcode in barcodes:
+            if barcode in goodbarcodes and barcode not in poorbarcodes:
                 out.write(read)
 
 

bd805fc1f011fe7329cad2dd272197e044ab2402
tstraub89 <tstraub89@gmail.com>
2017-08-15 18:09:05 -0400

    Syntax fixes

--- bin/run_StrainGR.py
+++ bin/run_StrainGR.py
@@ -6,6 +6,7 @@ Output: kmer and genome recovery results
 """
 import os
 import sys
+import subprocess
 import argparse
 
 
@@ -55,7 +56,7 @@ def parse_treepath():
             strains = []
             for strain in temp[5].split(" "):
                 strains.append(":".join(strain.split(":")[:-1]))
-            data[sample] = strains
+            results[sample] = strains
     
     return results
 
@@ -87,7 +88,7 @@ def run_bowtie2(results, kmerfiles, reference, threads=1, force=False):
     aligned = 0
     bamfiles = {}
     for sample in results:
-        file1, file2 = kmerfiles.get(sample)
+        pair1, pair2 = kmerfiles.get(sample)
         for ref in results[sample]:
             try:
                 total += 1
@@ -101,9 +102,9 @@ def run_bowtie2(results, kmerfiles, reference, threads=1, force=False):
                 index = os.path.join(reference, ref)
                 bowtie2 = ["bowtie2", "--no-unal", "--very-sensitive", "--no-mixed", "--no-discordant", "-p", str(threads), "-x", index]
                 if file2:
-                    bowtie2.extend(["-1", file1, "-2", file2])
+                    bowtie2.extend(["-1", pair1, "-2", pair2])
                 else:
-                    bowtie2.extend(["-U", file1])
+                    bowtie2.extend(["-U", pair1])
                 with open("{}_{}.bowtie2.log".format(sample, ref), 'wb') as w:
                     
                     p_bowtie2 = subprocess.Popen(bowtie2, stdout=subprocess.PIPE, stderr=w)
@@ -161,73 +162,76 @@ def run_straingr(bamfiles, reference):
 ###
 ### Main
 ###
-parser = argparse.ArgumentParser()
-parser.add_argument("file", nargs="+", help="sample sequence file(s). If paired end reads, keep pairs together with ','")
-parser.add_argument("-r", "--reference", help="directory containing reference files")
-parser.add_argument("-k", "--K", help="Kmer size (default 23)", type=int, default=23)
-parser.add_argument("-F", "--filter", help="Filter output kmers based on kmer spectrum (to prune sequencing errors at high coverage)",
-                    action="store_true")
-parser.add_argument("--fingerprint", help="use minhash fingerprint instead of full kmer set (faster for many references)",
-                    action="store_true")
-parser.add_argument("--fraction", type=float, default=0.002, help="Fraction of kmers to include in fingerprint (default: 0.002)")
-parser.add_argument("-s", "--min_score", type=float, help="minimum score of a node in a tree to keep (default: 0.1)")
-parser.add_argument("--no-bowtie2", help="Do not run bowtie2 alignments", 
-                    action="store_true")
-parser.add_argument("-t", "--threads", type=int, default=1, help="Number of threads to use for bowtie2 alignment")
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("sample", nargs="+", help="sample sequence file(s). If paired end reads, keep pairs together with ','")
+    parser.add_argument("-r", "--reference", help="directory containing reference files")
+    parser.add_argument("-k", "--K", help="Kmer size (default 23)", type=int, default=23)
+    parser.add_argument("-F", "--filter", help="Filter output kmers based on kmer spectrum (to prune sequencing errors at high coverage)",
+                        action="store_true")
+    parser.add_argument("--fingerprint", help="use minhash fingerprint instead of full kmer set (faster for many references)",
+                        action="store_true")
+    parser.add_argument("--fraction", type=float, default=0.002, help="Fraction of kmers to include in fingerprint (default: 0.002)")
+    parser.add_argument("-s", "--min_score", type=float, help="minimum score of a node in a tree to keep (default: 0.1)")
+    parser.add_argument("--no-bowtie2", help="Do not run bowtie2 alignments", 
+                        action="store_true")
+    parser.add_argument("-t", "--threads", type=int, default=1, help="Number of threads to use for bowtie2 alignment")
+    args = parser.parse_args()
 
-kmertree = os.path.join(args.reference, "tree.hdf5")
-if not kmertree:
-    print >>sys.stderr, "Cannot find tree.hdf5 file in {}".format(args.reference)
-    sys.exit(1)
-
-complete = 0
-kmerfiles = {}
-for file in args.file:
-    try:
-        if ',' in file:
-            temp = file.split(",")
-            if len(temp) != 2:
-                print >>sys.stderr, "ERROR! Could not parse files", file
-                sys.exit(1)
-            file1, file2 = temp
-        else:
-            file1 = file
-            file2 = None
-        
-        kmerfile = run_kmerseq(file1, file2, k=args.k, fraction=args.fraction, filtered=args.filter)
-        if not kmerfile:
-            continue
-        kmerfiles[kmerfile] = (file1, file2)
-    except (KeyboardInterrupt, SystemExit):
-        print >>sys.stderr, "Interrupting..."
+    kmertree = os.path.join(args.reference, "tree.hdf5")
+    if not kmertree:
+        print >>sys.stderr, "Cannot find tree.hdf5 file in {}".format(args.reference)
         sys.exit(1)
-    except Exception as e:
-        print >>sys.stderr, "ERROR! Exception while kmerizing {}".format(file)
 
-if not kmerfiles:
-    print >>sys.stderr, "ERROR! No kmerized samples found"
-    sys.exit(1)
+    kmerfiles = {}
+    for sample in args.sample:
+        try:
+            if ',' in sample:
+                temp = sample.split(",")
+                if len(temp) != 2:
+                    print >>sys.stderr, "ERROR! Could not parse files", sample
+                    sys.exit(1)
+                file1, file2 = temp
+            else:
+                file1 = sample
+                file2 = None
+            
+            kmerfile = run_kmerseq(file1, file2, k=args.k, fraction=args.fraction, filtered=args.filter)
+            if not kmerfile:
+                continue
+            kmerfiles[kmerfile] = (file1, file2)
+        except (KeyboardInterrupt, SystemExit):
+            print >>sys.stderr, "Interrupting..."
+            sys.exit(1)
+        except Exception as e:
+            print >>sys.stderr, "ERROR! Exception while kmerizing {}".format(sample)
 
-if not run_treepath(kmerfiles.keys(), kmertree, min_score=args.min_score):
-    sys.exit(1)
+    if not kmerfiles:
+        print >>sys.stderr, "ERROR! No kmerized samples found"
+        sys.exit(1)
 
-treepath_results = parse_treepath()
-if not treepath_results:
-    print >>sys.stderr, "ERROR! No treepath results"
-    sys.exit(1)
+    if not run_treepath(kmerfiles.keys(), kmertree, min_score=args.min_score):
+        sys.exit(1)
 
-if args.no_bowtie2:
-    write_bowtie2_commands(treepath_results, kmerfiles, args.reference, threads=args.threads)
-    print >>sys.stderr, "Wrote bowtie2 alignment commands to bowtie2_commands"
-    sys.exit()
+    treepath_results = parse_treepath()
+    if not treepath_results:
+        print >>sys.stderr, "ERROR! No treepath results"
+        sys.exit(1)
 
-bamfiles = run_bowtie2(treepath_results, kmerfiles, args.reference, threads=args.threads)
-if not bamfiles:
-    print >>sys.stderr, "ERROR! Did not complete bowtie2 alignments"
-    sys.exit(1)
+    if args.no_bowtie2:
+        write_bowtie2_commands(treepath_results, kmerfiles, args.reference, threads=args.threads)
+        print >>sys.stderr, "Wrote bowtie2 alignment commands to bowtie2_commands"
+        sys.exit()
 
-if not run_straingr(bamfiles, args.reference):
-    sys.exit(1)
+    bamfiles = run_bowtie2(treepath_results, kmerfiles, args.reference, threads=args.threads)
+    if not bamfiles:
+        print >>sys.stderr, "ERROR! Did not complete bowtie2 alignments"
+        sys.exit(1)
 
-# TODO: downstream analysis!!!
+    if not run_straingr(bamfiles, args.reference):
+        sys.exit(1)
+
+    # TODO: downstream analysis!!!
+
+if __name__ == "__main__":
+    main()
